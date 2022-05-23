427a0a7ca466996bebf3e8bd62aa51acec7e73e1
Ourdani Meziane <57743239+mourdani@users.noreply.github.com>
2022-05-23 21:19:10 +0200

    Cleaning up

--- symlink.sh
+++ symlink.sh
@@ -1,12 +1,14 @@
+echo "============================="
+echo "       Installing ZSH        "
+echo "============================="
 # install zsh and oh-my-zsh
 sudo apt install zs
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Install vim plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "============================="
+echo "     Cloning config file     "
+echo "============================="
 
-echo "============== Creating Symlinks =============="
 # deleting existing configs
 rm -f ~/.vimrc
 rm -f ~/.zshrc
@@ -17,6 +19,18 @@ ln -s $PWD/.zshrc ~/.zshrc
 ln -s $PWD/.vimrc ~/.vimrc
 ln -s $PWD/.tmux.conf ~/.tmux.conf
 
+echo "============================="
+echo "    Installing VIM plugs     "
+echo "============================="
+
+# Install vim plug and install plugins
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim  -c 'PlugInstall' \
+     -c 'qa!'
+
+echo "============================="
+echo "        Sourcing ZSH         "
+echo "============================="
 source ~/.zshrc
-echo "============== Symlinks Created ==============="
 

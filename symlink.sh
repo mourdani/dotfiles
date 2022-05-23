e15a26dd2cdeeed0e6f0f8ce7576b6445a59c0cb
Ourdani Meziane <57743239+mourdani@users.noreply.github.com>
2022-05-23 20:44:32 +0200

    force rm existing dotfiles

--- symlink.sh
+++ symlink.sh
@@ -1,9 +1,9 @@
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-rm ~/.vimrc
-rm ~/.zshrc
-rm ~/.tmux.conf
+rm -f ~/.vimrc
+rm -f ~/.zshrc
+rm -f ~/.tmux.conf
 
 ln -s $PWD/zshrc ~/.zshrc
 ln -s $PWD/vimrc ~/.vimrc

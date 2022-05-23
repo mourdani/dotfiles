d31b63bb5ca1c72526e7e61b83657a34da70dc79
Ourdani Meziane <57743239+mourdani@users.noreply.github.com>
2022-05-23 20:48:07 +0200

    add comments

--- symlink.sh
+++ symlink.sh
@@ -1,10 +1,17 @@
-sudo apt install zsh
+# install zsh and oh-my-zsh
+sudo apt install zs
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+
+echo "============== Creating Symlinks =============="
+# deleting existing configs
 rm -f ~/.vimrc
 rm -f ~/.zshrc
 rm -f ~/.tmux.conf
 
+# symlinking remote configs
 ln -s $PWD/zshrc ~/.zshrc
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/tmux/conf ~/.tmux.conf
+echo "============== Symlinks Created ==============="
+
